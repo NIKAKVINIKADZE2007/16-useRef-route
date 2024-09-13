b6de810cb81785cd556ce5353c782e5ebdccb2a5
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-09-13 21:16:58 +0400

    useRef/example2

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const App2 = () => {
-  return <div>App2</div>;
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+
+  const nameInputRef = useRef(null);
+
+  const surnameInputRef = useRef(null);
+
+  const handlekeyup1 = (e) => {
+    console.log(e);
+    if (e.key === 'Enter') {
+      surnameInputRef.current.focus();
+    }
+  };
+
+  const handlekeyup2 = (e) => {
+    if (e.key === 'Enter') {
+      nameInputRef.current.focus();
+    }
+  };
+
+  console.log(nameInputRef);
+  return (
+    <div>
+      <form>
+        <label htmlFor='myName'>Name</label>
+        <input
+          id='myName'
+          ref={nameInputRef}
+          type='text'
+          placeholder='Name'
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          onKeyUp={handlekeyup1}
+        />
+
+        <label htmlFor='mySurname'>surname</label>
+        <input
+          id='mySurname'
+          ref={surnameInputRef}
+          type='text'
+          placeholder='surname'
+          value={surname}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          onKeyUp={handlekeyup2}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default App2;
---
+++ src/App3.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from 'react';
+
+const App3 = () => {
+  const [name, setName] = useState('');
+  const [showPopup, setShowPopup] = useState(true);
+
+  const nameInputRef = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowPopup(false);
+  };
+
+  useEffect(() => {
+    if (showPopup && nameInputRef.current) nameInputRef.current.focus();
+  }, []);
+  return (
+    <>
+      {showPopup && (
+        <div className='popup'>
+          <div className='popup-content'>
+            <h2>Your Name</h2>
+            <form onSubmit={handleSubmit}>
+              <input
+                ref={nameInputRef}
+                type='text'
+                placeholder='Enter your name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <button type='submit'>Submit</button>
+            </form>
+          </div>
+        </div>
+      )}
+      {!showPopup && (
+        <div className='main-content'>
+          <h1>Welcome, {name}!</h1>
+          {/* Other content */}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default App3;
